28909a69fa96912af2140e02a1ab3af692b63d02
prathamesh207 <prathameshlad207@gmail.com>
2024-01-11 03:12:00 +0530

    code commit

---
+++ Edtech/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Edtech.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Edtech.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+
+
+    }
+}
---
+++ Edtech/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿namespace Edtech.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string CourseTitle { get; set; }
+
+        public string InstructorName { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+
+        public int Price { get; set; }
+
+
+
+    }
+}
